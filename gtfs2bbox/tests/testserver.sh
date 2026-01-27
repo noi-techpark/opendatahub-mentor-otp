b4016e7194ea5ecbe752dfc8509c3a5529503334
Clemens Zagler <c.zagler@noi.bz.it>
2026-01-27 14:51:01 +0100

    chore: cleanup unused v1 services

--- gtfs2bbox/tests/testserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#
-echo 'listen http://localhost:9090/'
-ncat -k -l -p 9090 -c "echo 'HTTP/1.1 200 OK\nContent-type: application/json\nconnection: close\\n'; echo '{\"status\":true}'"
--- journey/src/util/user.js
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-export function isNewUser (loggedInUser) {
-  return !loggedInUser.hasConsentedToTerms
-}
