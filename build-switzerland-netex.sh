605205e6b460ea6dcbb7edb2bd23fb1a723af9d8
Leonard Ehrenfried <mail@leonard.io>
2026-04-14 16:12:27 +0200

    Fix script

--- build-switzerland-netex.sh
+++ build-switzerland-netex.sh
@@ -9,17 +9,23 @@ CURL="curl --location --fail --show-error -#"
 INPUT_FILE=switzerland.netex.zip
 OUTPUT_FILE=switzerland.epip.netex.zip
 
-git clone --branch binary_relation_serializer git@github.com:MMTIS/badger.git
+if [ ! -d "badger" ]; then
+  git clone --branch binary_relation_serializer git@github.com:MMTIS/badger.git
+fi
 
 cd badger
 
 uv sync
-source venv/bin/activate
+UV_VENV_CLEAR=1 sh scripts/setup.sh
 
-curl https://data.opentransportdata.swiss/dataset/timetablenetex_2026/permalink -o ${INPUT_FILE}
+source .venv/bin/activate
+
+${CURL} https://data.opentransportdata.swiss/dataset/timetablenetex_2026/permalink -o ${INPUT_FILE}
+
+echo "Starting to convert Swiss NeTEx data to EPIP"
 
 uv run python -m conv.netex_to_db switzerland.netex.zip switzerland.lmdb
 uv run python -m conv.epip_db_to_db switzerland.lmdb switzerland-epip.lmdb
 uv run python -m conv.epip_db_to_xml switzerland-epip.lmdb switzerland-epip.xml
 
-zip switzerland.epip.xml.zip ${OUTPUT_FILE} --junk-paths
+zip ${OUTPUT_FILE} switzerland-epip.xml --junk-paths
