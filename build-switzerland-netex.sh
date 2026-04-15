7fca069e83f09b2f431b2da1161936f38b9b5237
Leonard Ehrenfried <mail@leonard.io>
2026-04-15 10:17:28 +0200

    Add script to convert Swiss NeTEx data to EPIP (#283)
    * Add script for building Swiss NeTEx data
    
    * Fix script
    
    * Add more variables

---
+++ build-switzerland-netex.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+# SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+CURL="curl --location --fail --show-error -#"
+
+INPUT_FILE=switzerland.netex.zip
+OUTPUT_FILE=switzerland.epip.netex.xml
+OUTPUT_ZIP_FILE=switzerland.epip.netex.zip
+
+if [ ! -d "badger" ]; then
+  git clone --branch binary_relation_serializer git@github.com:MMTIS/badger.git
+fi
+
+cd badger
+
+uv sync
+UV_VENV_CLEAR=1 sh scripts/setup.sh
+
+source .venv/bin/activate
+
+${CURL} https://data.opentransportdata.swiss/dataset/timetablenetex_2026/permalink -o ${INPUT_FILE}
+
+echo "Starting to convert Swiss NeTEx data to EPIP"
+
+uv run python -m conv.netex_to_db switzerland.netex.zip switzerland.lmdb
+uv run python -m conv.epip_db_to_db switzerland.lmdb switzerland-epip.lmdb
+uv run python -m conv.epip_db_to_xml switzerland-epip.lmdb ${OUTPUT_FILE}
+
+zip ${OUTPUT_ZIP_FILE} ${OUTPUT_FILE} --junk-paths
