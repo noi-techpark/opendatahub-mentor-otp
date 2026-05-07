34a4a08242871350e4ca66214274cdb4cf8a428c
Clemens Zagler <c.zagler@noi.bz.it>
2026-05-07 10:46:34 +0200

    pelias production merge

--- pelias/importers/fetch-poi.js
+++ pelias/importers/fetch-poi.js
@@ -20,7 +20,9 @@ if (!fs.existsSync(DATA_DIR)){
 }
 
 // Query stops from OTP graphql endpoint
-GraphqlOtp.query('https://v2.otp.opendatahub.com/otp/gtfs/v1', GraphqlOtp.queries.getAllPoi)
+const OTP_GRAPH_URL = process.env.OTP_GRAPH_URL || 'https://v2.otp.opendatahub.com/otp/gtfs/v1';
+console.log(`Using OTP instance: ${OTP_GRAPH_URL}`);
+GraphqlOtp.query(OTP_GRAPH_URL, GraphqlOtp.queries.getAllPoi)
     .then((data) => {
         let stops = data.data.stops;
         let stations = data.data.stations;
